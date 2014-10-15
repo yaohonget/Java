4b10a9d3e7377733313172664d872f40dde81d96
yaohonget <yaohonget@gmail.com>
2014-10-15 10:31:03 +0200

    - add hello world project

---
+++ HelloWorld/src/com/hong/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.hong.helloworld;
+
+public class HelloWorld {
+	
+	public void sayHello () {
+		System.out.println("Hello, World!");
+	}
+	
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.sayHello();
+	}
+	
+}
