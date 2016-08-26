630f75480ba394cae71804f4ac8fca5f3dd28b4a
hyao <hyao@intershop.de>
2016-08-26 17:09:18 +1000

    - update for consumer producer problem

--- Paralle/src/com/hong/paralle/sync/ConsumerProducer.java
+++ Paralle/src/com/hong/paralle/sync/ConsumerProducer.java
@@ -1,62 +1,83 @@
 package com.hong.paralle.sync;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * http://www.journaldev.com/1061/thread-safety-in-java
+ * 
+ * @author HYao
+ *
+ */
 public class ConsumerProducer {
 
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		Bank bank = new Bank();
 		Consumer con = new Consumer();
 		Producer pro = new Producer();
-		
-		ExecutorService exService = Executors.newFixedThreadPool(10);//newSingleThreadScheduledExecutor();//Executors.newFixedThreadPool(10);
-		Collection<Callable<Integer>> tasks = Arrays.asList(con.save(bank), pro.draw(bank)); 
-		
+
+		ExecutorService exService = Executors.newFixedThreadPool(10);// newSingleThreadScheduledExecutor();//Executors.newFixedThreadPool(10);
+
+		Collection<Callable<Integer>> tasks = Arrays.asList(con.save(bank), pro.draw(bank));
+
 		try {
 			exService.invokeAll(tasks);
+			exService.shutdownNow();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
 }
 
 class Consumer {
 	int save = 0;
-		
+
 	Callable<Integer> save(Bank b) {
-		return () -> {save++; b.save();return 1;};
+		return () -> {
+			synchronized (b) {
+				save++;
+				b.save();
+				return 1;
+			}
+		};
 	}
 }
 
 class Producer {
 	int draw = 0;
-	
+
 	Callable<Integer> draw(Bank b) {
-		return () -> {draw++; b.draw();return 1;};
+		return () -> {
+			synchronized (b) {
+				draw++;
+				b.draw();
+				return 1;
+			}
+		};
 	}
 }
 
 class Bank {
-	
+
+	public Object lock;
+
 	public int amount = 0;
-	
-	synchronized void save() {
+
+	void save() {
+		// synchronized (lock) {
 		amount++;
 		System.out.println("save 1, amount : " + amount);
+		// }
 	}
-	
-	synchronized void draw() {
+
+	void draw() {
+		// synchronized(lock) {
 		amount--;
 		System.out.println("draw 1, amount : " + amount);
+		// }
 	}
 }
