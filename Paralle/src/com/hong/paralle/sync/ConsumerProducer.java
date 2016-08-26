ad51a9b9183fd6d1800a90b2d7351a7ea84690cb
hyao <hyao@intershop.de>
2016-08-26 12:45:46 +1000

    - consumer / producer

--- Paralle/src/com/hong/paralle/sync/ConsumerProducer.java
+++ Paralle/src/com/hong/paralle/sync/ConsumerProducer.java
@@ -1,11 +1,29 @@
 package com.hong.paralle.sync;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ConsumerProducer {
+
 	public static void main(String [] args) {
 		Bank bank = new Bank();
 		Consumer con = new Consumer();
 		Producer pro = new Producer();
 		
+		ExecutorService exService = Executors.newFixedThreadPool(10);//newSingleThreadScheduledExecutor();//Executors.newFixedThreadPool(10);
+		Collection<Callable<Integer>> tasks = Arrays.asList(con.save(bank), pro.draw(bank)); 
+		
+		try {
+			exService.invokeAll(tasks);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
@@ -14,21 +32,31 @@ public class ConsumerProducer {
 
 class Consumer {
 	int save = 0;
+		
+	Callable<Integer> save(Bank b) {
+		return () -> {save++; b.save();return 1;};
+	}
 }
 
 class Producer {
 	int draw = 0;
+	
+	Callable<Integer> draw(Bank b) {
+		return () -> {draw++; b.draw();return 1;};
+	}
 }
 
 class Bank {
 	
-	int amount = 0;
+	public int amount = 0;
 	
-	void save() {
-		System.out.println("");
+	synchronized void save() {
+		amount++;
+		System.out.println("save 1, amount : " + amount);
 	}
 	
-	void draw() {
-		System.out.println("");
+	synchronized void draw() {
+		amount--;
+		System.out.println("draw 1, amount : " + amount);
 	}
 }
